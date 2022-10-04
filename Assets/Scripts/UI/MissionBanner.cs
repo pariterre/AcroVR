0f0d13dc83bfe3b637ac85012e284e651694fd90
Pariterre <pariterre@hotmail.com>
2022-10-04 15:19:23 -0400

    Made the MissionBanner clickable via a button instead of anywhere

--- Assets/Scripts/UI/MissionBanner.cs
+++ Assets/Scripts/UI/MissionBanner.cs
@@ -4,47 +4,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public delegate void EndOfMissionCallback(); //
+public delegate void ClosedMissionBannerCallback();
 
 public class MissionBanner : MonoBehaviour
 {
     protected Animator animator; 
     protected Text text;
+    protected Button continueButton;
     public bool IsShown { get; protected set; } = false;
 
+    public ClosedMissionBannerCallback closedCallback;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         text = GetComponentInChildren<Text>();
+        var allButtons = GetComponentsInChildren<Button>();
+        continueButton = allButtons[0];
     }
 
     public void SetText(string _newText){
         text.text = _newText;
     }
 
-    public void Show(EndOfMissionCallback _closedCallback = null){
+    public void Show(ClosedMissionBannerCallback _closedCallback = null){
         animator.Play("Panel In");
         IsShown = true;
-        StartCoroutine(WaitClickToCloseBanner(_closedCallback));
+        closedCallback = _closedCallback;
     }
 
     public void Hide(){
         animator.Play("Panel Out");
         IsShown = false;
+        if (closedCallback != null)
+            closedCallback();
+
     }
 
-    IEnumerator WaitClickToCloseBanner(EndOfMissionCallback _closedCallback){
-        while (true){
-            if (Input.anyKeyDown)
-            {
-                Hide();
-                if (_closedCallback != null)
-                    _closedCallback();
-                break;
-            }
-            yield return null;
-        }
+    public void ClickedContinue(){
+        Debug.Log("Coucou");
+        if (!IsShown) return;
+        Hide();
     }
 }
